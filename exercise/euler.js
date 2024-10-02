9dd0638db6514225f1f27aca89efc73135fffbc7
Tintu Mariuyam Paul <60310275+TintuMariyamPaul@users.noreply.github.com>
2024-10-02 16:43:56 +0100

    changes made

--- exercise/euler.js
+++ exercise/euler.js
@@ -33,6 +33,13 @@ function cal(){
 
 
     const result =  sumOfMult(a,b,n);
+    const resultDivision = document.getElementById('result');
+    resultDivision.textContent = `The sum of multiples of numbers ${a} or ${b} below ${n} is: ${result}`;
+
+    document.getElementById('a').style.display = 'none';
+    document.getElementById('b').style.display = 'none';
+    document.getElementById('n').style.display = 'none';
+    document.getElementById('clickbutton').style.display = 'none';
 
     alert(`The sum of multiples of numbers ${a} or ${b} below ${n} is: ${result}`);
 }
