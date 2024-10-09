68fa7ee2f516289ce97cb1ea307ee27f0f705283
Tintu Mariuyam Paul <60310275+TintuMariyamPaul@users.noreply.github.com>
2024-10-09 17:19:38 +0100

    commit

--- exercise/array.js
+++ exercise/array.js
@@ -1,15 +1,14 @@
-let l = [1,2,3,4,5,6,7,8,9,10];
+let l = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 let a = 2;
 let b = 3;
 
 function sumMultiples(a, b, l) {
     return l.reduce((sum, num) => {
-       if (num % a === 0 || num % b === 0) {
-           return sum + num;
+        if (num % a === 0 || num % b === 0) {
+            return sum + num;
         }
         return sum;
     }, 0);
 }
-
-let result = sumMultiples(a,b,l);
+let result = sumMultiples(a, b, l);
 alert(`The sum of multiples of numbers ${a} or ${b} is: ${result}`);
