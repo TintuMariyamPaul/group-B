7f446ed2f2b243aea9a375995afed4a397050651
Tintu Mariuyam Paul <60310275+TintuMariyamPaul@users.noreply.github.com>
2024-10-09 15:38:18 +0100

    import

--- exercise/array.js
+++ exercise/array.js
@@ -1,3 +1,7 @@
+let l = [1,2,3,4,5,6,7,8,9,10];
+let a = 2;
+let b = 3;
+
 function sumMultiples(a, b, l) {
   let sum = 0;
   for (let num of l) {
@@ -8,13 +12,5 @@ function sumMultiples(a, b, l) {
   return sum;
 }
 
-function calculateSum() {
-  const a = parseInt(document.getElementById("a").value);
-  const b = parseInt(document.getElementById("b").value);   
-
-  const listInput = document.getElementById("list").value;
-  const l = listInput.split(",").map(Number);
-
-  const result = sumMultiples(a, b, l);
-  document.getElementById("result").textContent = `The sum of multiples of ${a} or ${b} in the list is: ${result}`;
-}
+let result = sumMultiples(a,b,1);
+alert(`The sum of multiples of numbers ${a} or ${b} below ${l} is: ${result}`);
