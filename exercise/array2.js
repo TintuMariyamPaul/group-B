4f5ef77bd813581963c694881cb5a3daffccc835
Tintu Mariuyam Paul <60310275+TintuMariyamPaul@users.noreply.github.com>
2024-10-09 18:18:20 +0100

    push correct

--- exercise/array2.js
+++ exercise/array2.js
@@ -1,17 +1,15 @@
 
-let l = [1,2,3,4,5,6,7,8,9,10];
-let a = [2,3];
-
+let l = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let a = [2, 3];
 
 function sumMultiples(a, l) {
   return l.reduce((sum, num) => {
-      if (num % a[0] === 0 || num % a[l] === 0) {
-          return sum + num;
-      }
-      return sum;
+    if (num % a[0] === 0 || num % a[1] === 0) { 
+      return sum + num;
+    }
+    return sum;
   }, 0);
-
 }
 
-let result = sumMultiples(a,l);
+let result = sumMultiples(a, l);
 alert(`The sum of multiples of ${a[0]} or ${a[1]} in the list is: ${result}`);
