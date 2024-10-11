39496b4dafae5ec9c9fa431c3e195d2f6d711c67
Tintu Mariuyam Paul <60310275+TintuMariyamPaul@users.noreply.github.com>
2024-10-11 12:25:11 +0100

    push

---
+++ exercise/basket.js
@@ -0,0 +1,12 @@
+let a = (basket,prices)=>{
+   let calculateTotalCost = 0;
+   For(let item in basket){
+    if(prices[item] != undefined){
+        calculateTotalCost += basket[item]*prices[item];
+    }
+   }
+   return calculateTotalCost;
+}
+basket = {orange:1, apple:5, grapes:3};
+prices = {orange:1.02, apple:0.05,grapes:2.0, tea: 5.0 , milk:8.0};
+alert(a(basket,prices));
