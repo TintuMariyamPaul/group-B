370e8a739972b8890658cef18e46b67ec815a1a6
Tintu Mariuyam Paul <60310275+TintuMariyamPaul@users.noreply.github.com>
2024-10-11 12:56:08 +0100

    push

--- exercise/basket.js
+++ exercise/basket.js
@@ -7,6 +7,6 @@ let a = (basket,prices) =>{
    }
    return calculateTotalCost;
 }
-basket = {orange:1, apple:2, grapes:3};
-prices = {orange:1.02, apple:0.05,grapes:2.0, coffee: 3.0 , bread:2.0};
+basket = {orange:1, apple:2, grapes:3, coffee:1, bread:1};
+prices = {orange:1.02, apple:0.05, grapes:2.0, coffee: 3.0 , bread:2.0};
 alert(a(basket,prices));
